aed9cda397ecaddfe4ef917c087ca872d4fe24f3
cameronconway3 <49353534+cameronconway3@users.noreply.github.com>
2020-10-14 11:07:03 +0100

    building files

---
+++ routes/books.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const Book = require('../models').Book;
+
+// Handler function to wrap each route
+function asyncHandler(cb){
+    return async(req, res, next) => {
+        try {
+            await cb(req, res, next)
+        } catch(error){
+            // Forward error to the global error handler
+            next(error);
+        }
+    }
+}
+
+
+
+// GET /books - Show the full list of books
+router.get("/books", asyncHandler(async (req, res) => {
+    // Get all books in database
+    const books = await Book.findAll({ order: [['createdAt', 'DESC']]});
+    res.render()
+}));
+
+
+// GET /books/new - Show the create new book form
+router.get("/books/new", asyncHandler(async (req, res) => {
+
+}));
+
+
+// POST /books/new - Posts a new book to the database
+
+
+
+// GET /books/:id - Shows book detail form
+router.get("/books/:id", asyncHandler(async (req, res) => {
+
+}));
+
+
+// POST /books/:id - Updates book info in the database
+
+
+
+// POST /books/:id/delete - Deletes a book.
+
+
+module.exports = router;
